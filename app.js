3c9c3c8b027fec456dfc8f5fbccb1d896705bf9f
Eylexander <eylexander88@gmail.com>
2023-02-28 16:09:58 +0100

    v2

--- app.js
+++ app.js
@@ -4,39 +4,29 @@ const axios = require('axios');
 const moment = require('moment');
 const log = message => {console.log(`[${moment().format('MM-DD HH:mm:ss.SSS')}] ${message}`)};
 
-const hostname = '127.0.0.1';
-const port = 8888;
+const port = 8080;
 
-const server = http.createServer(async (req, res) => {
+const requestListener = async function (req, res) {
     log('Request for ' + req.url + ' by method ' + req.method + ' on status ' + res.statusCode);
 
-    if (req.method == 'GET') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+    const { data } = await axios.get('https://eylexander.xyz/memes/folder.json');
 
-        const { data } = await axios.get('https://eylexander.xyz/Project-1D/folder.json');
+    const contents = data.map(o => o.contents);
+    const getFile = contents[0].map(o => o.name);
+    const getRandomMeme = getFile[Math.floor(Math.random() * getFile.length)];
 
-        const contents = data.map(o => o.contents);
-        const getFile = contents[0].map(o => o.name);
-        const getRandomMeme = getFile[Math.floor(Math.random() * getFile.length)]
+    const response = { meme: `https://eylexander.xyz/memes/${getRandomMeme}` };
 
-        if (getRandomMeme === 'folder.json') {
-            getRandomMeme = 'rest_here.mp4';
-        }
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(response));
+}
 
-        const response = {
-            meme: `https://eylexander.xyz/Project-1D/${getRandomMeme}`
-        };
+const server = http.createServer(requestListener);
 
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.write(JSON.stringify(response));
-        res.end();
-    }
-});
-
-server.listen(port, hostname, () => {
-    log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    log(`Server running on port : ${port}`);
 });
